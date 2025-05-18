05cf8dfdcaba7228fb39a241662475af9b70a104
yurii_fishbakh <bunbanto@gmail.com>
2025-05-18 17:27:53 +0200

    for deploy

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,24 +1,120 @@
 import css from './MovieModal.module.css';
+import { useEffect, useCallback } from 'react';
+import type { Movie } from '../../types/movie';
+import { createPortal } from 'react-dom';
 
-<div className={css.backdrop} role="dialog" aria-modal="true">
-  <div className={css.modal}>
-    <button className={css.closeButton} aria-label="Close modal">
-      &times;
-    </button>
-    <img
-      src="https://image.tmdb.org/t/p/original/backdrop_path"
-      alt="movie_title"
-      className={css.image}
-    />
-    <div className={css.content}>
-      <h2>movie_title</h2>
-      <p>movie_overview</p>
-      <p>
-        <strong>Release Date:</strong> movie_release_date
-      </p>
-      <p>
-        <strong>Rating:</strong> movie_vote_average/10
-      </p>
-    </div>
-  </div>
-</div>;
+// {/* <div className={css.backdrop} role="dialog" aria-modal="true">
+//   <div className={css.modal}>
+//     <button className={css.closeButton} aria-label="Close modal">
+//       &times;
+//     </button>
+//     <img
+//       src="https://image.tmdb.org/t/p/original/backdrop_path"
+//       alt="movie_title"
+//       className={css.image}
+//     />
+//     <div className={css.content}>
+//       <h2>movie_title</h2>
+//       <p>movie_overview</p>
+//       <p>
+//         <strong>Release Date:</strong> movie_release_date
+//       </p>
+//       <p>
+//         <strong>Rating:</strong> movie_vote_average/10
+//       </p>
+//     </div>
+//   </div>
+// </div>; */}
+interface MovieModalProps {
+  movie: Movie | null;
+  onClose: () => void;
+}
+
+export default function MovieModal({ movie, onClose }: MovieModalProps) {
+  const handleBackDropClick = useCallback(
+    (evt: React.MouseEvent<HTMLDivElement>) => {
+      if (evt.target === evt.currentTarget) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+  useEffect(() => {
+    function handleEscKey(evt: KeyboardEvent) {
+      if (evt.key === 'Escape') {
+        onClose();
+      }
+    }
+    document.addEventListener('keydown', handleEscKey);
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+      document.body.style.overflow = '';
+    };
+  }, [onClose]);
+
+  if (movie == null) {
+    return createPortal(
+      <div
+        onClick={handleBackDropClick}
+        className={css.backdrop}
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className={css.modal}>
+          <button
+            onClick={onClose}
+            className={css.closeButton}
+            aria-label="Close modal"
+          >
+            &times;
+          </button>
+          <div className={css.content}>
+            <h2>Oops!, something gonna wrong.</h2>
+            <p>No movie data available.</p>
+          </div>
+        </div>
+      </div>,
+      document.body
+    );
+  }
+  return createPortal(
+    <div
+      onClick={handleBackDropClick}
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className={css.modal}>
+        <button
+          onClick={onClose}
+          className={css.closeButton}
+          aria-label="Close modal"
+        >
+          &times;
+        </button>
+        <img
+          src={
+            movie.backdrop_path !== null
+              ? `https://image.tmdb.org/t/p/original${movie?.backdrop_path}`
+              : placeholderImage
+          }
+          alt={movie.title}
+          className={css.image}
+        />
+        <div className={css.content}>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+          <p>
+            <strong>Rating:</strong> {movie.vote_average.toFixed(2)}/10
+          </p>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+}
